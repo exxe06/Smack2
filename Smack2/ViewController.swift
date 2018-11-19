ef1fd92a899aa984a3678f4b2f3403b24e01bed4
exxe <exxe06@marc-terences-macbook-pro.local>
2018-11-19 17:28:33 +0100

    Initial Commit

---
+++ Smack2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack2
+//
+//  Created by exxe on 19.11.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
